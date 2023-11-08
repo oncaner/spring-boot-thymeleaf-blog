a7f197eaff4b9a806ea120a6d87e857beb78ae74
Caner <caneron6@gmail.com>
2023-11-08 09:19:41 +0300

    first commit

---
+++ src/test/java/caner/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package caner.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
